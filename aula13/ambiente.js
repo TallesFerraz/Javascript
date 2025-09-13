50ed4cc6eface0631d9627b3717dfdaf9f5d06d2
TallesFerraz <tallesferraz.tf@gmail.com>
2025-09-12 22:20:50 -0300

    estrutura basica de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 10)
+    //executa primeiro e testa depois
+
+/*
+var cont = 1
+while (cont <= 5) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+    TESTA Primeiro e executa depois
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
